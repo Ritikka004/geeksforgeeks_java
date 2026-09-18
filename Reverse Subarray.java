14b913627c0cf8d3cd3d6edb6e7452be6ced4cc9
Ritikka004 <ritikkajaganathan@gmail.com>
2026-09-18 06:34:06 +0530

    Add Java solution for Reverse Subarray.
    This solution reverses the elements within the given subarray range using the two-pointer technique. It converts the 1-based indices to 0-based indices and swaps elements from both ends until the pointers meet. The reversal is performed in place. The implementation runs in O(r - l) time with O(1) extra space.

---
+++ Reverse Subarray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static ArrayList<Integer> reverseSubArray(ArrayList<Integer> arr, int l,
+                                                     int r) {
+        l--;
+        r--;
+        while(l < r){
+            int temp = arr.get(l);
+            arr.set(l,arr.get(r));
+            arr.set(r,temp);
+            l++;
+            r--;
+        }
+        return arr;
+    }
+}
