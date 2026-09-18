0b4cc618cd98adac0c4fda1bdfe52e71acf58097
Ritikka004 <ritikkajaganathan@gmail.com>
2026-09-18 05:59:28 +0530

    Add Java solution for Sum Except First and Last.
    This solution calculates the sum of all elements in an integer array except the first and last elements. It iterates from index 1 to the second-last index and adds each element to the running sum. The implementation runs in O(n) time and uses O(1) extra space.

---
+++ Sum Except First and Last.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int sumExceptFirstLast(int[] arr) {
+        int sum = 0;
+        for(int i = 1; i < arr.length - 1;i++){
+            sum += arr[i];
+        }
+        return sum;
+    }
+}
